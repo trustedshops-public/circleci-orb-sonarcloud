d0dd65c74e02873c8c51248620dc1b6bac975a89
Timo Reymann <Timo.Reymann@trustedshops.de>
2021-12-21 11:20:23 +0100

    feat: Add initial orb

---
+++ src/scripts/run.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+export SONAR_TOKEN="$<<parameters.sonar_token_variable_name>>"
+export SONAR_USER_HOME=$SCANNER_DIRECTORY/.sonar
+
+SCANNER_DIRECTORY=/tmp/cache/scanner
+OS="linux"
+
+if [[ ! -x "${SCANNER_DIRECTORY}/sonar-scanner-${PARAM_VERSION}-${OS}/bin/sonar-scanner" ]]; then
+  curl -Ol "https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-${PARAM_VERSION}-${OS}.zip"
+  unzip -qq -o "sonar-scanner-cli-${PARAM_VERSION}-${OS}.zip" -d ${SCANNER_DIRECTORY}
+fi
+
+chmod +x "${SCANNER_DIRECTORY}/sonar-scanner-${PARAM_VERSION}-${OS}/bin/sonar-scanner"
+chmod +x "${SCANNER_DIRECTORY}/sonar-scanner-${PARAM_VERSION}-${OS}/jre/bin/java"
+
+SCANNER_BIN="${SCANNER_DIRECTORY}/sonar-scanner-${PARAM_VERSION}-${OS}/bin/sonar-scanner"
+
+ # Report quality status on pull requests
+if [[ -n $CIRCLE_PULL_REQUEST ]];
+then
+  PR_ARGS="-Dsonar.pullrequest.key=${CIRCLE_PULL_REQUEST##*/} \
+           -Dsonar.pullrequest.base=${PARAM_DEFAULT_BRANCH} \
+           -Dsonar.pullrequest.provider=${PARAM_PR_PROVIDER} \
+           -Dsonar.pullrequest.branch=${CIRCLE_BRANCH} \
+           -Dsonar.pullrequest.github.repository=${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}"
+fi
+
+# shellcheck disable=SC2086
+${SCANNER_BIN} ${PR_ARGS}
