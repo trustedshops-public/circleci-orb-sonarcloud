e49ee7ac98e7ec541d50f3f1442e5c76aa484a50
Timo Reymann <Timo.Reymann@trustedshops.de>
2021-12-21 11:54:28 +0100

    fix: Fix sonar token variable

--- src/scripts/run.sh
+++ src/scripts/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export SONAR_TOKEN="$<<parameters.sonar_token_variable_name>>"
+export SONAR_TOKEN="${PARAM_SONAR_TOKEN}"
 export SCANNER_DIRECTORY=/tmp/cache/scanner
 export SONAR_USER_HOME=$SCANNER_DIRECTORY/.sonar
 export OS="linux"
